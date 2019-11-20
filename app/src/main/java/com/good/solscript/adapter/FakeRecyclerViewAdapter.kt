e09cf8709bc09a695230a223ce97b2d1907c6b55
ganghee <idiosauri@naver.com>
2019-11-20 18:15:19 +0900

    conflict solve

--- app/src/main/java/com/good/solscript/adapter/FakeRecyclerViewAdapter.kt
+++ app/src/main/java/com/good/solscript/adapter/FakeRecyclerViewAdapter.kt
@@ -4,46 +4,29 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.good.solscript.R
-<<<<<<< HEAD
 import com.good.solscript.data.FakeResponse
-
-class FakeRecyclerViewAdapter : RecyclerView.Adapter<FakeViewHolder>() {
-
-    private val fakeList = mutableListOf<FakeResponse>()
-=======
 import com.good.solscript.data.SampleData
 
 class FakeRecyclerViewAdapter : RecyclerView.Adapter<FakeViewHolder>() {
 
-    private val fakeList = mutableListOf<SampleData>()
->>>>>>> eb8c9932daa90427a2b8d4273829a25f26247277
+        private val fakeList = mutableListOf<SampleData>()
 
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FakeViewHolder =
-        FakeViewHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.recyclerview_fake_item,
-                parent,
-                false
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FakeViewHolder =
+            FakeViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.recyclerview_fake_item,
+                    parent,
+                    false
+                )
             )
-        )
+        override fun getItemCount(): Int = fakeList.size
 
-<<<<<<< HEAD
-=======
+        override fun onBindViewHolder(holder: FakeViewHolder, position: Int) =
+            holder.bind(fakeList[position])
 
->>>>>>> eb8c9932daa90427a2b8d4273829a25f26247277
-    override fun getItemCount(): Int = fakeList.size
-
-    override fun onBindViewHolder(holder: FakeViewHolder, position: Int) =
-        holder.bind(fakeList[position])
-
-<<<<<<< HEAD
-    fun setData(setDataList: List<FakeResponse>) {
-=======
-    fun setData(setDataList: List<SampleData>) {
->>>>>>> eb8c9932daa90427a2b8d4273829a25f26247277
-        this.fakeList.clear()
-        this.fakeList.addAll(setDataList)
-        notifyDataSetChanged()
+        fun setData(setDataList: List<SampleData>) {
+            this.fakeList.clear()
+            this.fakeList.addAll(setDataList)
+            notifyDataSetChanged()
+        }
     }
-}
--- app/src/main/java/com/good/solscript/adapter/FakeViewHolder.kt
+++ app/src/main/java/com/good/solscript/adapter/FakeViewHolder.kt
@@ -2,11 +2,8 @@ package com.good.solscript.adapter
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
-<<<<<<< HEAD
 import com.good.solscript.data.FakeResponse
-=======
 import com.good.solscript.data.SampleData
->>>>>>> eb8c9932daa90427a2b8d4273829a25f26247277
 import kotlinx.android.synthetic.main.recyclerview_fake_item.view.*
 
 
@@ -16,11 +13,7 @@ class FakeViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
     private val title = itemView.textview_title
     private val userId = itemView.textview_userId
 
-<<<<<<< HEAD
-    fun bind(fakeData: FakeResponse){
-=======
     fun bind(fakeData: SampleData){
->>>>>>> eb8c9932daa90427a2b8d4273829a25f26247277
         body.text = fakeData.body
         identify.text = fakeData.id.toString()
         title.text = fakeData.title
